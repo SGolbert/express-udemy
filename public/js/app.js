4682ff75856fa9d998859eb0318bcfa239a08190
Sebastian Golbert <sebastian.golbert@gmail.com>
2020-07-19 23:05:20 +0200

    implement client rendering for the search

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
-console.log('Client side javascript file is loaded!')
+console.log("Client side javascript file is loaded!");
+
+const weatherForm = document.querySelector("form");
+const result = document.querySelector(".temperature");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = e.target[0].value;
+
+  const url = "http://localhost:3000/weather?address=" + searchTerm;
+
+  fetch(url).then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    response.json().then((data) => {
+      if (data.error) {
+        result.innerHTML = data.error;
+        console.error(data.error);
+      } else {
+        result.innerHTML = `Temperature right now: ${data.forecast.temperature}°C`;
+      }
+    });
+  });
+});
--- src/app.js
+++ src/app.js
@@ -24,14 +24,14 @@ app.use(express.static(publicDirectoryPath));
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather",
-    name: "Andrew Mead",
+    name: "Sebastian Golbert",
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About Me",
-    name: "Andrew Mead",
+    name: "Sebastian Golbert",
   });
 });
 
@@ -39,7 +39,7 @@ app.get("/help", (req, res) => {
   res.render("help", {
     helpText: "This is some helpful text.",
     title: "Help",
-    name: "Andrew Mead",
+    name: "Sebastian Golbert",
   });
 });
 
@@ -69,7 +69,6 @@ app.get("/weather", (req, res) => {
 
           res.send({
             forecast: forecastData,
-            location: location,
             address: req.query.address,
           });
         });
@@ -81,7 +80,7 @@ app.get("/weather", (req, res) => {
 app.get("/help/*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Andrew Mead",
+    name: "Sebastian Golbert",
     errorMessage: "Help article not found.",
   });
 });
@@ -89,7 +88,7 @@ app.get("/help/*", (req, res) => {
 app.get("*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Andrew Mead",
+    name: "Sebastian Golbert",
     errorMessage: "Page not found.",
   });
 });
